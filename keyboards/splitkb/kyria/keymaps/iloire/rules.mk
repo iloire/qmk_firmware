44eca67f9044afa0a6a16a8b160b4eeb461a88e0
Ivan Loire <(none)>
2021-11-16 12:21:38 +0100

    feat: add kyria profile for iloire

---
+++ keyboards/splitkb/kyria/keymaps/iloire/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+AUTO_SHIFT_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+RGBLIGHT_ENABLE = no      # Enable keyboard RGB underglow
