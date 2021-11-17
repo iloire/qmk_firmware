6f674ae318242fd85387189f03a4c52fcaa93938
Ivan Loire <(none)>
2021-11-17 09:23:56 +0100

    feat: more tweaks

--- keyboards/splitkb/kyria/keymaps/iloire/keymap.c
+++ keyboards/splitkb/kyria/keymaps/iloire/keymap.c
@@ -48,24 +48,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_QWERTY] = LAYOUT(
      LT(NAV, KC_ESC), KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_MINS,
-     KC_LSFT , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
+     LSFT_T(KC_TAB) , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
      CTL_ESC , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSLASH,
-                                ADJUST , KC_LGUI, KC_SPC, ALT_ENT , NAV   ,     SYM    , KC_BSPC ,KC_SFTENT, LT(NAV, KC_EQL), _______
+                                KC_LALT , KC_LGUI, KC_SPC, ALT_ENT , NAV   ,     SYM    , KC_BSPC ,KC_SFTENT, LT(NAV, KC_EQL), _______
     ),
 
 
     [_COLEMAK_DH] = LAYOUT(
      LT(NAV, KC_ESC), KC_Q ,  KC_W   ,  KC_F  ,   KC_P ,   KC_B ,                                        KC_J,   KC_L ,  KC_U ,   KC_Y ,KC_SCLN, KC_MINS,
-     CTL_ESC , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSLASH,
-                                 ADJUST, KC_LGUI, KC_SPC, ALT_ENT , NAV   ,     SYM    , KC_SPC ,KC_SFTENT, LT(NAV, KC_EQL), _______
+     LSFT_T(KC_TAB)  , KC_A ,  KC_R   ,  KC_S  ,   KC_T ,   KC_G ,                                        KC_M,   KC_N ,  KC_E ,   KC_I ,  KC_O , CTL_QUOT,
+     CTL_ESC , KC_Z ,  KC_X   ,  KC_C  ,   KC_D ,   KC_V , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_K,   KC_H ,KC_COMM, KC_DOT ,KC_SLSH, KC_BSLASH,
+                                 KC_LALT, KC_LGUI, KC_SPC, ALT_ENT , NAV   ,     SYM    , KC_SPC ,KC_SFTENT, LT(NAV, KC_EQL), _______
     ),
 
     [_NAV] = LAYOUT(
       _______, _______, _______, KC_LPRN, KC_RPRN, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
       _______, _______, _______, KC_LCBR, KC_RCBR, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
       _______, _______, _______, KC_LBRC, KC_RBRC, _______, _______, KC_SLCK, _______, _______,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
-                                 KC_SCLN, KC_EQL, _______, _______, _______, _______, _______, _______, _______, _______
+                                 ADJUST, KC_EQL, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 
